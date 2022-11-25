b640431fa5cec4514daa4a23cfecb12fc3ebdafb
Arthurbhs <arthur.shekinarfoxy@gmail.com>
2022-11-25 09:23:44 -0300

    exercicio 3

---
+++ funMEAN/Ex3/Ex3.js
@@ -0,0 +1,13 @@
+let num = 1
+
+
+
+
+
+function  showNext() {
+
+console.log(num++)
+
+}
+
+module.exports = { showNext }
---
+++ funMEAN/Ex3/Ex3_2.js
@@ -0,0 +1,8 @@
+const s1 = require("./Ex3")
+
+const s2 = require("./Ex3")
+
+s1.showNext()
+s2.showNext()
+s1.showNext()
+s2.showNext()
